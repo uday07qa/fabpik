9564b6dd47c37e16d7475677e49a062b9ad60ca2
uday <udayqa07@gmail.com>
2023-02-08 20:24:08 +0530

    mahesh1

---
+++ appiumm code/src/main/java/scroll/examscrolling.java
@@ -0,0 +1,17 @@
+package scroll;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class examscrolling {
+
+	public static void main(String[] args) {
+
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("platformName", "android");
+		capabilities.setCapability("device", "Nexus 5X API 30");
+		capabilities.setCapability("appPackage", "Nexus 5X API 30");
+		capabilities.setCapability("device", "Nexus 5X API 30");
+		capabilities.setCapability("device", "Nexus 5X API 30");
+	}
+
+}
